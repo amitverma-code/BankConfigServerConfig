bfc669d96dc8ae5a3b5196b8e68c7ad399895277
844198 <844198@LTIN188927.cts.com>
2020-08-14 00:26:13 +0530

    Commit1

---
+++ BankConfig/src/main/java/com/bank/management/system/BankConfigApplication.java
@@ -0,0 +1,18 @@
+package com.bank.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
+public class BankConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankConfigApplication.class, args);
+	}
+
+}
